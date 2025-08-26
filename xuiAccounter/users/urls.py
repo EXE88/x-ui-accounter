fb754aca32f275015707f919f13066074f4c1c5e
EXE <bobolk1388@gmail.com>
2025-08-26 18:30:56 +0330

    Rename UserSignUp view to UserRegister for consistency in user registration flow

--- xuiAccounter/users/urls.py
+++ xuiAccounter/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.UserSignUp.as_view(), name="user_register"),
+    path("register/", views.UserRegister.as_view(), name="user_register"),
     path("login/", views.UserLogin.as_view(), name="user_login"),
 ]
