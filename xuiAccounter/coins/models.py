1eede491004b378a6511a5553ba4d48c3f80da26
EXE <bobolk1388@gmail.com>
2025-09-01 01:17:24 +0330

    Add unique constraint to user field in Coin model

--- xuiAccounter/coins/models.py
+++ xuiAccounter/coins/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Coin(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, unique=True)
     number_of_coins = models.PositiveIntegerField()
