fafd6a4a43b6e15543e744197bea0576ebe237c0
EXE <bobolk1388@gmail.com>
2025-09-01 01:47:50 +0330

    Add Plan model with admin configuration and initial migration

--- xuiAccounter/plans/admin.py
+++ xuiAccounter/plans/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Plan
 
-# Register your models here.
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('plan_name', 'number_of_users_with_unit', 'time_with_unit', 'usage_with_unit', 'price_with_unit')
+    
+    search_fields = ('plan_name',)
+    list_filter = ('time', 'number_of_users')
+    ordering = ('plan_name',)
+    list_per_page = 20
+
+    def number_of_users_with_unit(self,obj):
+        return f"{obj.number_of_users} کاربره"
+    number_of_users_with_unit.short_description = "NUMBER OF USERS"
+
+    def time_with_unit(self, obj):
+        return f"{obj.time} ماه"
+    time_with_unit.short_description = "TIME (month)"
+
+    def usage_with_unit(self, obj):
+        return f"{obj.usage} GB"
+    usage_with_unit.short_description = "USAGE (GB)"
+
+    def price_with_unit(self, obj):
+        return f"{obj.price} کوین"
+    price_with_unit.short_description = "PRICE (Coins)"
--- xuiAccounter/plans/models.py
+++ xuiAccounter/plans/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Plan(models.Model):
+    plan_name = models.CharField(max_length=150,blank=False,null=False,help_text="نام پلن")
+    number_of_users = models.PositiveIntegerField(blank=False,null=False,help_text="تعداد کاربران کانفیگ")
+    time = models.PositiveIntegerField(blank=False,null=False,help_text="مدت زمان کانفیگ بر حسب ماه")
+    usage = models.PositiveIntegerField(blank=False,null=False,help_text="حجم کانفیگ بر حسب گیگابایت")
+    price = models.PositiveIntegerField(blank=False,null=False,help_text="قیمت کانفیگ بر حسب کوین (هر کوین هزار تومن)")
