f36e3433cfbfe8103de0b18b40c1941caa0099e4
EXE <bobolk1388@gmail.com>
2025-09-01 23:17:55 +0330

    Add GetPlans view and URL routing for plans

--- xuiAccounter/plans/urls.py
+++ xuiAccounter/plans/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path("", views.GetPlans.as_view(), name="get_plans"),
 ]
--- xuiAccounter/plans/views.py
+++ xuiAccounter/plans/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Plan
 
-# Create your views here.
+class GetPlans(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        all_plans = Plan.objects.all()
+        plans_data = []
+        for plan in all_plans:
+            plans_data.append({
+                "id": plan.id,
+                "plan_name": plan.plan_name,
+                "number_of_users": plan.number_of_users,
+                "time": plan.time,
+                "usage": plan.usage,
+                "price": plan.price,
+            })
+        return Response({"plans": plans_data}, status=200)
+            
